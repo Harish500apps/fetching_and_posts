8ccbde20d3c6a7bc6befbd2e3fd7bdf1e47d779a
Harish <141122572+Harish500apps@users.noreply.github.com>
2023-08-01 17:05:38 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Count from './components/Count.vue'
+</script>
+<template>
+  <div>
+    <h2>Project Details </h2>
+    <Count/>
+
+  </div>
+  
+</template>
---
+++ Count.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+const count=ref(0)
+
+function add(){
+  count.value++
+}
+</script>
+
+<template>
+<div>
+  <div>{{ count }}</div>
+  <button @click="add">Click</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
